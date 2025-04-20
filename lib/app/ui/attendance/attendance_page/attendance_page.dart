fdfd8aed59b15adf2cecf7307ad61ec288797035
Luvianto <126235306+Luvianto@users.noreply.github.com>
2025-04-20 08:46:57 +0700

    Update attendance_page.dart

--- lib/app/ui/attendance/attendance_page/attendance_page.dart
+++ lib/app/ui/attendance/attendance_page/attendance_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sunday_school_attendance/app/common/widgets/custom_card.dart';
+import 'package:sunday_school_attendance/app/common/widgets/custom_loading.dart';
 import 'package:sunday_school_attendance/app/ui/attendance/attendance_page/attendance_controller.dart';
 
 class AttendancePage extends GetView<AttendanceController> {
@@ -8,38 +9,36 @@ class AttendancePage extends GetView<AttendanceController> {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() {
-      if (controller.isLoading.value) {
-        return const Center(child: CircularProgressIndicator());
-      }
-      if (controller.errorMessage.isNotEmpty) {
-        return Center(child: Text(controller.errorMessage.value));
-      }
-      if (controller.attendanceList.isEmpty) {
-        return const Center(child: Text("Belum ada data!"));
-      }
-      return Scaffold(
-        body: RefreshIndicator(
-          onRefresh: controller.refreshPage,
-          child: ListView(
-            physics: const AlwaysScrollableScrollPhysics(),
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            children: controller.attendanceList.map(
-              (attendance) {
-                return CustomCard(
-                  title: attendance.name,
-                  subtitle: '${attendance.startTime} - ${attendance.endTime}',
-                  onTap: () => controller.toDetail(attendance),
-                );
-              },
-            ).toList(),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: controller.openForm,
-          child: Icon(Icons.add),
-        ),
-      );
-    });
+    return RefreshIndicator(
+      onRefresh: controller.refreshPage,
+      child: Stack(
+        children: [
+          Obx(() {
+            return ListView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              children: controller.attendanceList
+                  .map((attendance) => CustomCard(
+                        title: attendance.sessionType.name,
+                        subtitle: attendance.timestamp.toString(),
+                        onTap: () => controller.toDetail(attendance),
+                      ))
+                  .toList(),
+            );
+          }),
+          Obx(() {
+            if (controller.isLoading.value) {
+              return CustomLoading();
+            }
+            return Center(
+              child: Text(
+                controller.errorMessage.value,
+                textAlign: TextAlign.center,
+              ),
+            );
+          })
+        ],
+      ),
+    );
   }
 }
