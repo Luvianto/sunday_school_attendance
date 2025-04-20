92ddd1c43c3aa8a6525973e773711467dcaa07e5
Luvianto <126235306+Luvianto@users.noreply.github.com>
2025-04-20 08:48:16 +0700

    Added AttendanceCard

---
+++ lib/app/common/widgets/attendance_card.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class AttendanceCard extends StatefulWidget {
+  final String title;
+  final void Function()? onTap;
+
+  const AttendanceCard({
+    super.key,
+    required this.title,
+    this.onTap,
+  });
+
+  @override
+  State<AttendanceCard> createState() => _AttendanceCardState();
+}
+
+class _AttendanceCardState extends State<AttendanceCard> {
+  bool isPresent = false;
+
+  void togglePresent(void Function()? onTap) {
+    setState(() {
+      isPresent = !isPresent;
+    });
+    if (onTap != null) onTap();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 12),
+      child: InkWell(
+        onTap: () => togglePresent(widget.onTap),
+        child: Row(
+          children: [
+            Container(
+              width: 4,
+              height: 64,
+              decoration: BoxDecoration(
+                color: isPresent ? Colors.green : Colors.red,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(8.0),
+                  bottomLeft: Radius.circular(8.0),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.title,
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    const SizedBox(height: 4.0),
+                    Text(
+                      isPresent ? 'Hadir' : 'Alpha',
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color:
+                                Theme.of(context).colorScheme.onSurfaceVariant,
+                          ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
