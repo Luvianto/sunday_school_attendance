6133834ad1b43eb3588f96f8422203046bcfc276
Luvianto <126235306+Luvianto@users.noreply.github.com>
2025-04-20 08:42:43 +0700

    Added status to StudenModel

--- lib/app/models/student_model.dart
+++ lib/app/models/student_model.dart
@@ -1,25 +1,29 @@
 class StudentModel {
   final String? id;
   final String name;
+  final bool status; // Whether the student is still active or not
   final String? profilePictureUrl;
 
   StudentModel({
     this.id,
     required this.name,
+    required this.status,
     this.profilePictureUrl,
   });
 
-  factory StudentModel.fromJson(Map<String, dynamic> json, String? id) {
+  factory StudentModel.fromFirestore(Map<String, dynamic> json, String? id) {
     return StudentModel(
       id: id,
       name: json['name'],
+      status: json['status'],
       profilePictureUrl: json['profile_picture_url'],
     );
   }
 
-  Map<String, dynamic> toJson() {
+  Map<String, dynamic> toFirestore() {
     return {
       'name': name,
+      'status': status,
       'profile_picture_url': profilePictureUrl ?? '',
     };
   }
