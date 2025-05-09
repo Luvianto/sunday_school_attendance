d96b0d13c88be535148e5eeaa7272fc7cb78206b
Luvianto <126235306+Luvianto@users.noreply.github.com>
2025-05-09 23:07:14 +0700

    Update attendance_detail_controller.dart

--- lib/app/ui/attendance/attendance_detail/attendance_detail_controller.dart
+++ lib/app/ui/attendance/attendance_detail/attendance_detail_controller.dart
@@ -1,19 +1,40 @@
 import 'package:get/get.dart';
-import 'package:sunday_school_attendance/app/models/session_model.dart';
+import 'package:sunday_school_attendance/app/models/attendance_model.dart';
+import 'package:sunday_school_attendance/app/services/attendance_service.dart';
 
 class AttendanceDetailController extends GetxController {
+  final attendanceService = AttendanceService();
+
   var isLoading = true.obs;
   var errorMessage = ''.obs;
   var isEdited = false.obs;
-  var attendance = Rxn<SessionModel>();
+  var attendance = Rxn<AttendanceModel>();
 
   @override
-  void onInit() async {
+  void onInit() {
     super.onInit();
-    isLoading.value = false;
+    fetchAttendance();
   }
 
   Future<void> refreshPage() async {}
+
+  void fetchAttendance() async {
+    isLoading.value = true;
+    final argument = Get.arguments;
+    if (argument is AttendanceModel) {
+      final result = await attendanceService.getAttendance(argument.id!);
+      if (result.isSuccess && result.isNotEmpty) {
+        attendance.value = result.data!;
+      }
+      if (result.isEmpty) {
+        errorMessage.value = 'Data tidak ditemukan!\nKembali ke halaman utama!';
+      }
+      if (result.isError) {
+        errorMessage.value = result.message!;
+      }
+    }
+    isLoading.value = false;
+  }
 
   void closePage() {
     Get.back(result: isEdited);
